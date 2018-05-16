5ddb04a5c9e4f3e966abbdb72c5f6995cf8f807e
Angelina Wong <angelina.w25@gmail.com>
2018-05-16 19:15:15 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else 
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
 
   end
 
   def count_sentences
-
+    counter =0
+    sent = self.split
+    sent.each do |sent|
+    if sent.question? | sent.sentence? | sent.exclamation?
+      counter +=1
+    end
+    end
+    counter
   end
 end
