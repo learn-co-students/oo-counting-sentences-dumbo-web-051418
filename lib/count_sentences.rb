810da77bffb0800349460e2368a0f9baef8782f0
Jathmel Gordon <jathmel@gmail.com>
2018-05-16 20:08:57 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self[-1] == '.'
+    false
   end
 
   def question?
+    return true if self[-1] == '?'
+    false
 
   end
 
   def exclamation?
-
+     return true if self[-1] == '!'
+    false
   end
 
   def count_sentences
-
+   self.split(/[.?!]/).select {|sentence| sentence != ""}.count
   end
 end
